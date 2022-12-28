676f323a3db178f8cfcf3bb33231fa9e9ed2fc35
Maks_Travel <maks.traveler@gmail.com>
2022-12-28 19:41:13 +0300

    new

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-file = open(r'Exorde.txt',)
-s=0
-for i in file:
-        for j in i:
-                print(j)
+def file_n_lines(file_name: str, n: int) -> None:
+        f = open(file_name, 'r')
+        for i in range(n):
+             print(f.readline(), end='')
+
+
+file_n_lines('test.txt', 3)
