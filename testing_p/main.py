cee7913d27498059b5f4f0088add1f6b94f99dcf
Maks_Travel <maks.traveler@gmail.com>
2022-12-29 16:59:21 +0300

    new

--- testing_p/main.py
+++ testing_p/main.py
@@ -1,10 +1,13 @@
-def create_file_with_numbers(n):
-    file = open('range_'+str(n)+'.txt', 'w')
-    for i in range(1, n+1):
-        file.write(str(i)+'\n')
-    file.close()
+def find_lines_len_more_6(file_name:str) -> int:
+    with open(file_name, 'r') as f:
+        s= [i for i in f.read().splitlines()]
+        x= 0
+        for i in s:
+            if len(i) >6:
+                x+=1
+        return x
 
 
-create_file_with_numbers(5)
+
 
 
