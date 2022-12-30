212ca6eb3e946732f2d2364ca0b7dafe83e338ad
Maks_Travel <maks.traveler@gmail.com>
2022-12-30 20:02:56 +0300

    new

--- testing_p/main.py
+++ testing_p/main.py
@@ -1,13 +1,10 @@
-def find_lines_len_more_6(file_name:str) -> int:
-    with open(file_name, 'r') as f:
-        s= [i for i in f.read().splitlines()]
-        x= 0
-        for i in s:
-            if len(i) >6:
-                x+=1
-        return x
+employees = [
+    'Pankratiy', 'Innokentiy', 'Anfisa', 'Yaroslava', 'Veniamin',
+    'Leonti', 'Daniil', 'Mishka', 'Lidochka',
+    'Terenti', 'Vladik', 'Svetka', 'Maks', 'Yura', 'Sergei'
+]
 
+identifiers = [77, 48, 88, 85, 76, 81, 62, 43, 5, 56, 17, 20, 37, 32, 96]
 
-
-
-
+employees_data = dict(zip(sorted(identifiers), sorted(employees)))
+print(employees_data)
