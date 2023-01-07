7bc58342cc09f02c3ca37dbd7308a1d5cca7255d
Maks_Travel <maks.traveler@gmail.com>
2023-01-07 21:44:35 +0300

    new

--- testing_p/main.py
+++ testing_p/main.py
@@ -1,7 +1,9 @@
-def print_from(n: int) -> None:
-        if n>1:
-                print_from(n-3)
-        print(n)
+n = list(map(int, input().split()))
+def get_rec_sum(n):
+    '''Функция вычисления суммы списка'''
+    if len(n) ==1: #проверяем длинну списка
+        return n[0]
+    else:
+        return n[0] + get_rec_sum(n[1:])  # извлекаем первое значение из списка и к нему плюсуем оставшийся список
 
 
-print_from(5)
