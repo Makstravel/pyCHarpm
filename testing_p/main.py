dca391da4545fb55480c2e6ced396bc164ff21b3
Maks_Travel <maks.traveler@gmail.com>
2023-01-09 23:35:51 +0300

    new

--- testing_p/main.py
+++ testing_p/main.py
@@ -1,9 +1,2 @@
-n = list(map(int, input().split()))
-def get_rec_sum(n):
-    '''Функция вычисления суммы списка'''
-    if len(n) ==1: #проверяем длинну списка
-        return n[0]
-    else:
-        return n[0] + get_rec_sum(n[1:])  # извлекаем первое значение из списка и к нему плюсуем оставшийся список
-
-
+def is_string(lst):
+    return all(map(lambda x: x==list,lst))
