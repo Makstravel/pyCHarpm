027fb52a0225d7bec555eaa7597430f86d2cebbe
Maks_Travel <maks.traveler@gmail.com>
2023-01-11 00:42:06 +0300

    new

--- testing_p/main.py
+++ testing_p/main.py
@@ -1,2 +1,10 @@
-def is_string(lst):
-    return all(map(lambda x: x==list,lst))
+def counter_add(k):
+    def plus_value():
+        return k+5
+
+    return plus_value
+
+cnt= counter_add()
+k = int(input())
+print(cnt())
+
