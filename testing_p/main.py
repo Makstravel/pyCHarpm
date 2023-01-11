f91a8b59ae6ad71550e871ed09e4f10efbe2d1b7
Maks_Travel <maks.traveler@gmail.com>
2023-01-12 00:08:20 +0300

    new

--- testing_p/main.py
+++ testing_p/main.py
@@ -1,20 +1,5 @@
+def double_it(func):
+    def doubl(*args, **kwargs):
+        func(*args, **kwargs)
 
-def text_decor(wfunc):
-    def wrap():
-        print('Hello')
-        wfunc()
-        print('Goodbye')
-    return wrap
-
-@text_decor
-def simple_func():
-    print('I just simple python func')
-
-simple_func()
-
-
-@text_decor
-def multiply(num1, num2):
-    print(num1 * num2)
-
-multiply(3, 5)
+    return doubl*doubl
