96543e61ce38aa297ff66e8d2c904a394963ccab
Maks_Travel <maks.traveler@gmail.com>
2023-01-17 19:32:09 +0300

    new

--- OOP/main.py
+++ OOP/main.py
@@ -1,19 +1,6 @@
-class Person:
-    name = "John Smith"
-    age = 30
-    gender = "male"
-    address = "123 Main St"
-    phone_number = "555-555-5555"
-    email = "johnsmith@example.com"
-    is_employed = True
+class Config:
+    pass
 
 
-s = input().split()
-for i in s:
-    if hasattr(Person, i.lower()):
-        print(f' {i}-YES')
-    else:
-        print(f' {i}-NO')
-
-
-
+def create_instance(n: int) -> Config:
+    pass
