60173fb80a1d278d7c671d3804350c4a53f12d2c
Maks_Travel <maks.traveler@gmail.com>
2023-01-23 22:49:54 +0300

    new

---
+++ OOP/egorov/2.3.22.py
@@ -0,0 +1,16 @@
+class Stack:
+    def __init__(self):
+        self.values = []
+
+    def push(self, item):
+        self.values.append(item)
+    def pop(self):
+        self.push.pop()
+
+    def peek(self):
+        return  self.values
+
+    def is_empty(self):
+        isinstance(Stack,)
+
+    def size(self):
