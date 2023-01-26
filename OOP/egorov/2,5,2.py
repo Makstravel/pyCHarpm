c239fddf210b50fcc1cba917189d2990207978f7
Maks_Travel <maks.traveler@gmail.com>
2023-01-26 19:52:07 +0300

    new

---
+++ OOP/egorov/2,5,2.py
@@ -0,0 +1,22 @@
+class UserMail:
+    def __init__(self, login, email):
+        self.login = login
+        self.__email = email
+
+    def get_email(self):
+        return self.__email
+
+    def set_email(self, val):
+        if isinstance(val, str) and '@' in val and '.' in val and val.count('@') == 1 and val.find('@') < val.find('.'):
+            self.__email = val
+        else:
+            print(f'ErrorMail:{val}')
+
+    email = property(fget=get_email, fset=set_email)
+
+k = UserMail('belosnezhka', 'prince@wait.you')
+print(k.email)  # prince@wait.you
+k.email = [1, 2, 3] # ErrorMail:[1, 2, 3]
+k.email = 'prince@still@.wait'  # ErrorMail:prince@still@.wait
+k.email = 'prince@still.wait'
+print(k.email)  # prince@still.wait
