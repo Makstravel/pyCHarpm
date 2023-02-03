c04d225d60b29b6b11bf3feab0b169c0c292e39a
Maks_Travel <maks.traveler@gmail.com>
2023-02-03 22:49:46 +0300

    new

--- OOP/egorov/2.3.2.py
+++ OOP/egorov/2.3.2.py
@@ -1,19 +1,41 @@
-class Dog:
+class Stack:
 
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+    def __init__(self):
+        self.values = []
 
-    def description(self):
-        return f'{self.name} is {self.age} years old'
+    def push(self, item):
+        self.values.append(item)
 
-    def speak(self, sound):
-        self.sound = sound
-        return f'{self.name} says {self.sound}'
+    def pop(self):
+        if len(self.values) != 0:
+            return self.values.pop()
+        else:
+            print("Empty Stack")
+
+    def peek(self):
+        if not self.is_empty():
+            return self.values[-1]
+        else:
+            print("Empty Stack")
+            return None
+
+    def is_empty(self):
+        return self.size() == 0
+
+    def size(self):
+        return len(self.values)
 
 
-jack = Dog("Jack", 4)
-
-print(jack.description()) # распечатает 'Jack is 4 years old'
-print(jack.speak("Woof Woof")) # распечатает 'Jack says Woof Woof'
-print(jack.speak("Bow Wow")) # распечатает 'Jack says Bow Wow'
+s = Stack()
+s.peek()  # распечатает 'Empty Stack'
+print(s.is_empty())  # распечатает True
+s.push('cat')  # кладем элемент 'cat' на вершину стека
+s.push('dog')  # кладем элемент 'dog' на вершину стека
+print(s.peek())  # распечатает 'dog'
+s.push(True)  # кладем элемент True на вершину стека
+print(s.size())  # распечатает 3
+print(s.is_empty())  # распечатает False
+s.push(777)  # кладем элемент 777 на вершину стека
+print(s.pop())  # удаляем элемент 777 с вершины стека и печатаем его
+print(s.pop())  # удаляем элемент True с вершины стека и печатаем его
+print(s.size())  # распечатает 2
