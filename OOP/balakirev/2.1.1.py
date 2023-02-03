3146801a4c1ca38482eb57617e131b1b1ca0be2c
Maks_Travel <maks.traveler@gmail.com>
2023-02-03 13:24:43 +0300

    new

--- OOP/balakirev/2.1.1.py
+++ OOP/balakirev/2.1.1.py
@@ -3,9 +3,15 @@ class Clock:
         self.__time = time
 
     def set_time(self, tm):
+        if Clock.__check_time(tm) == True:
+            self.__time = tm
 
+    @classmethod
     def get_time(self):
+        Clock.__time = self.time
 
+
+    @classmethod
     def __check_time(self, tm):
         self.tm = tm
         if type(self.tm) in int and self.tm >=0 and self.tm < 100_000:
@@ -15,3 +21,5 @@ class Clock:
 
 
 clock = Clock(4530)
+clock.set_time(500)
+print(clock.get_time())
