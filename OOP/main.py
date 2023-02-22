5c7b62bece9ab320a93c2f4515ee57806345b2b6
Maks_Travel <maks.traveler@gmail.com>
2023-02-22 21:54:33 +0300

    new

--- OOP/main.py
+++ OOP/main.py
@@ -1,11 +1,6 @@
-class MyClass:
-    def __init__(self, x):
-        self.x = x
-
-    def __eq__(self, other):
-        return isinstance(other, MyClass) and self.x == other.x
-
-a = MyClass(1)
-b = MyClass(1)
-
-print(a == b)
+emal = input()
+s = emal.find('@')
+if '.' in emal[s:]:
+    print('Yes')
+else:
+    raise ValueError('введите верный логин')
